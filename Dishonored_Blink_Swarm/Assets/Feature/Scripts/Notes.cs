5af36aafe356e9a36d6a921e42b1810a855119fe
WicksWaker <sphaepetersen@gmail.com>
2023-05-11 02:10:22 -0700

    Stuff
    Energy Bar worked on

--- Dishonored_Blink_Swarm/Assets/Feature/Scripts/Notes.cs
+++ Dishonored_Blink_Swarm/Assets/Feature/Scripts/Notes.cs
@@ -7,10 +7,7 @@ public class Notes : MonoBehaviour
     ///1. need to finish powers- both need to be singletons
     ///2. Enemy movement needs to be a random back and forth w/ pauses & needs to be able to shrink and lose health in size until inactive
     ///also probably needs a list, also needs to interpolate to rats (all enemies must then interpolate to rats)
-    ///3. Blink needs to be able to interpolate to where portal is at
-    ///4. Swarm- Set active and inactive
-    ///active and inactive rats, will need to Boid
+    ///4. Swarm- Set active and inactive rats
     ///want rats to be able to jump to random heights
-    ///5. Player needs to be able to rotate via mouse (so camera can do so too)
     ///6. Energy needs to be shown- text based or bar w/ increase/ decrease scale (blue cube that increases and decreases on the y axis)
 }
--- Dishonored_Blink_Swarm/Assets/Feature/Scripts/Player/TestingInputSystem.cs
+++ Dishonored_Blink_Swarm/Assets/Feature/Scripts/Player/TestingInputSystem.cs
@@ -10,12 +10,19 @@ public class TestingInputSystem : MonoBehaviour
     private InputAction swarm;
     private InputAction blink;
     private InputAction jump;
+
+    public int maxEnergy = 100;
+    public int currentEnergy;
+    public EnergyBar energyBar;
+
     [SerializeField]
     private GameObject spawner;
     private GameObject portal;
 
     private void Awake()
     {
+        currentEnergy = maxEnergy;
+        energyBar.SetMaxEnergy(maxEnergy);
         playerInputActions = new PlayerInputActions();
         playerInputActions.Enable();
         rigidBody = GetComponent<Rigidbody>();
@@ -38,6 +45,11 @@ public class TestingInputSystem : MonoBehaviour
         Vector2 moveVec = playerInputActions.Player.Move.ReadValue<Vector2>();
         rigidBody.AddForce(new Vector3(moveVec.x, 0, moveVec.y) * 5f, ForceMode.Force);
 
+    }
+
+    private void Update()
+    {
+       
     }
 
     private void OnEnable()
@@ -69,12 +81,21 @@ public class TestingInputSystem : MonoBehaviour
     public void OnSwarm(InputAction.CallbackContext context)
     {
         spawner.GetComponent<ratMove>().ratSpawner();
+        loseEnergy(20);
     }
 
     //calls Blink
     public void OnBlink(InputAction.CallbackContext context)
     {
-        GetComponent<playerBlink>(); 
+        GetComponent<playerBlink>();
+        loseEnergy(20);
+    }
+
+    void loseEnergy(int lose)
+    {
+        currentEnergy -= lose;
+
+        energyBar.SetMaxEnergy(currentEnergy);
     }
 
     public void PrintText(string text)
